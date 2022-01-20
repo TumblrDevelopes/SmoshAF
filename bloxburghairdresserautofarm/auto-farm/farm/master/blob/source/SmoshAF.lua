8655d42950d2810da7e94c37fe1459d500d55b28
TumblrDevelopes <74624288+TumblrDevelopes@users.noreply.github.com>
2022-01-20 12:13:53 -0600

    Update SmoshAF.lua

--- bloxburghairdresserautofarm/auto-farm/farm/master/blob/source/SmoshAF.lua
+++ bloxburghairdresserautofarm/auto-farm/farm/master/blob/source/SmoshAF.lua
@@ -1,5 +1,6 @@
 local fegrth5d_ert6er = "e5t345EFV"
-if getgenv().key == fegrth5d_ert6er then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/DisguisedFox94/CodeHub/main/dahood.lua"))();
-	print('passed key system')
+_G.key = fegrth5d_ert6er
+if _G.key == fegrth5d_ert6er then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DisguisedFox94/CodeHub/main/dahood.lua"))();
+    print('passed key system')
 end
